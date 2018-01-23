a3a03eea61942caec6cb340ee7e40e58d6137aeb
Dawnight <weizhiqimail@foxmail.com>
2018-01-23 22:54:20 +0800

    修改获取到歌手数据的数据结构

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -7,6 +7,9 @@
 <script>
   import {getSingerList} from 'api/singer'
   import {ERR_OK} from 'api/config'
+  import Singer from 'common/js/singer'
+  const HOT_NAME = '热门'
+  const HOT_SINGRS_LEN = 10
   export default {
     data() {
       return {
@@ -21,9 +24,60 @@
         getSingerList().then((res) => {
           if (res.code === ERR_OK) {
             this.singers = res.data.list
-            console.log(this.singers)
+            console.log(this._normalizaSinger(this.singers))
           }
         })
+      },
+      _normalizaSinger(list) {
+        let map = {
+          hot: {
+            title: HOT_NAME,
+            items: []
+          }
+        }
+        list.forEach((item, index) => {
+          if (index < HOT_SINGRS_LEN) {
+            map.hot.items.push(new Singer({
+              id: item.Fsinger_mid,
+              name: item.Fsinger_name
+            }))
+          }
+          const key = item.Findex
+          if (!map[key]) {
+            map[key] = {
+              title: key,
+              items: []
+            }
+          }
+          map[key].items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        })
+        /***
+         * 此时得到的map是一个对象，对象的遍历是无序的
+         * 所以，需要进一步处理map
+         */
+        console.log('map')
+        console.log(map)
+        let hot = []
+        let ret = []
+        for (let key in map) {
+          let val = map[key]
+          if (val.title.match(/[a-zA-Z]/)) {
+            ret.push(val)
+          } else if (val.title === HOT_NAME) {
+            hot.push(val)
+          }
+        }
+        ret.sort((a, b) => {
+          return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+        })
+        console.log('ret')
+        console.log(ret)
+        console.log('hot.concat(ret)')
+        console.log(hot.concat(ret))
+        return hot.concat(ret)
       }
     }
   }
