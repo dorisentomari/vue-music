3d2a9469f769f37d142be50a02bbde501792d8b4
Dawnight <weizhiqimail@foxmail.com>
2018-01-27 13:56:04 +0800

    歌手页面，配置更多的歌手

--- src/api/singer.js
+++ src/api/singer.js
@@ -7,7 +7,24 @@ export function getSingerList() {
   const data = Object.assign({}, commonParams, {
     channel: 'singer',
     page: 'list',
-    key: 'j_woman_all',
+    // key: 'all_all_all', // 全部歌手
+    key: 'cn_man_all', // 华语男
+    // key: 'cn_woman_all', // 华语女
+    // key: 'cn_team_all', // 华语组合
+    // key: 'k_man_all', // 韩国男
+    // key: 'k_woman_all', // 韩国女
+    // key: 'k_team_all', // 韩国组合
+    // key: 'j_man_all', // 日本男
+    // key: 'j_woman_all', // 日本女
+    // key: 'j_team_all', // 日本组合
+    // key: 'eu_man_all', // 欧美男
+    // key: 'eu_woman_all', // 欧美女
+    // key: 'eu_team_all', // 欧美组合
+    // key: 'c_orchestra_all', // 乐团
+    // key: 'c_performer_all', // 演奏家
+    // key: 'c_composer_all', // 作曲家
+    // key: 'c_cantor_all', // 指挥家
+    // key: 'other_other_all', // 其他
     pagesize: 100,
     pagenum: 1,
     g_tk: 5381,
