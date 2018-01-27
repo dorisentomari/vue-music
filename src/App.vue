480e0fc6a5bff0cfb233d9aa5243fa266e0cf303
Dawnight <weizhiqimail@foxmail.com>
2018-01-27 23:10:09 +0800

    初步建立vuex管理模型

--- src/App.vue
+++ src/App.vue
@@ -5,22 +5,24 @@
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
+    <player></player>
   </div>
 </template>
 
 <script>
   import MHeader from 'components/m-header/m-header.vue'
   import Tab from 'components/tab/tab.vue'
+  import Player from 'components/player/player.vue'
   export default {
     name: 'App',
     components: {
       MHeader,
-      Tab
+      Tab,
+      Player
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import "~common/stylus/variable.styl";
-
+  @import "~common/stylus/variable.styl"
 </style>
--- static/commitRecords/commit21-30.md
+++ static/commitRecords/commit21-30.md
@@ -15,3 +15,12 @@
 #### 第24次提交:开始制作播放器**核心**
 + 在`common/js/config`文件中定义播放需要的不同模式
 + 在`store/getter`,`store/state`,`store/mutations`和`store/mutations-types`文件中定义播放器不同情况下需要的一些播放设置，包括`playing`正在播放，`fullScreen`全屏播放，`playlist`播放列表，`sequenceList`排序列表，`mode`播放模式，`currentIndex`当前索引和`currentSong`当前歌曲
+
+#### 第25次提交:建立`vuex`管理模型
++ 建立`player`组件，由于不受其他组件及路由的影响，所以直接放在`App.vue`文件内使用
++ 在`song-list`组件内发射出一个`select`事件
++ `song-list`的父组件`music-list`组件进行接收事件，通过`vuex`的`getActions`，对传递的数据进行管理
++ 在`store/actions`中建立一个方法对`select`事件进行管理
+
+
+
