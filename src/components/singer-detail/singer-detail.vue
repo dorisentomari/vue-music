2f861644e8c7beb77c8451b262f922634ac76a2a
Dawnight <weizhiqimail@foxmail.com>
2018-01-27 18:12:28 +0800

    创建music-list组件和song-list组件

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,7 +1,7 @@
 <template>
   <transition name="slide">
     <div class="singer-detail">
-      helo
+      <music-list :songs="songs" :bg-image="bgImage" :title="title"></music-list>
     </div>
   </transition>
 </template>
@@ -11,19 +11,27 @@
   import {getSingerDetail} from 'api/singer'
   import {ERR_OK} from 'api/config'
   import {createSong} from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
   export default {
     data() {
       return {
-        song: []
+        songs: []
       }
     },
     computed: {
+      title() {
+        return this.singer.name
+      },
+      bgImage() {
+        return this.singer.avator
+      },
       ...mapGetters([
         'singer'
       ])
     },
     created() {
       this._getSingerDetail()
+      console.log(this.singer)
     },
     methods: {
       _getSingerDetail() {
@@ -49,20 +57,14 @@
         })
         return ret
       }
+    },
+    components: {
+      MusicList
     }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
-  @import '~common/stylus/variable'
-  .singer-detail
-    position: fixed
-    z-index: 100
-    top: 0
-    left: 0
-    right: 0
-    bottom: 0
-    background: $color-theme
   .slide-enter-active, .slide-leave-active
     transition: all 0.3s
   .slide-enter, .slide-leave-to
--- static/commitRecords/commit21-30.md
+++ static/commitRecords/commit21-30.md
@@ -2,3 +2,7 @@
 #### 第21次提交:获取QQ音乐歌手详情信息
 + 在`api/singer`内创建一个获取歌手详情信息的方法
 + 在`singer-detail`组件内获取数据
+
+#### 第22次提交:创建`music-list`组件和`song-list`组件
++ 在`singer-detail`组件内部显示`music-list`组件
++ 在`music-list`组件内部显示`song-list`组件，并用`scroll`组件包裹，让`song-list`组件可以滚动
