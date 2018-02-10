1e21388a2dca4a6e20b050172ebed2da473d3ec3
Dawnight <weizhiqimail@foxmail.com>
2018-02-10 13:33:28 +0800

    修复confirm组件的样式问题

--- src/base/confirm/confirm.vue
+++ src/base/confirm/confirm.vue
@@ -2,7 +2,7 @@
   <transition name="confirm-fade">
     <div class="confirm" v-show="showFlag" @click.stop>
       <div class="confirm-wrapper">
-        <div class="confirm content">
+        <div class="confirm-content">
           <p class="text">{{text}}</p>
           <div class="operate">
             <div @click="cancel" class="operate-btn left">{{cancelBtnText}}</div>
@@ -10,7 +10,6 @@
           </div>
         </div>
       </div>
-      <p class="test">this is test</p>
     </div>
   </transition>
 </template>
